06316f61b31c9377e99d2be04c83ab9a85e69e01
MATI4S0 <mateusfrancapaula@gmail.com>
2026-03-18 15:21:44 -0300

    Add files via upload

---
+++ AULA5/ex02.php
@@ -0,0 +1,37 @@
+<?php
+
+$cidadania1 = [
+    "pessoa" => "Manuel de Madeireiros",
+    "endereco" => "Rua das Acácias",
+    "cidade" => "Foz do Iguaçu",
+    "UF" => "PR"
+];
+
+$cidadania2 = [
+    "pessoa" => "Juliana de Amaral",
+    "endereco" => "Rua dos Pinheros",
+    "cidade" => "Florianópolis",
+    "UF" => "SC"
+];
+
+$cidadania3 = [
+    "pessoa" => "Rodrigo Baidek",
+    "endereco" => "Rua Dom Predo I",
+    "cidade" => "Petrópolis",
+    "UF" => "RJ"
+];
+
+$cidadania4 = [
+    "pessoa" => "Fabiola da Silva",
+    "endereco" => "Rua Chile",
+    "cidade" => "Guarulhos",
+    "UF" => "SP"
+];
+
+
+
+$conjuntoCidadania = array($cidadania1, $cidadania2, $cidadania3, $cidadania4);
+
+foreach ($conjuntoCidadania as $conj) {
+    echo $conj["pessoa"] . " | " . $conj["endereco"] . " | " . $conj["cidade"] . " | " . $conj["UF"] . " \n ";
+}
